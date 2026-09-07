fa7ebc9a8f3b41e1f8735998cff8325fad02e47b
Bandile Zikalala <st10521550@rcconnect.edu.za>
2026-09-07 23:11:22 +0200

    Half of part 1, code is running perfectly

---
+++ src/main/java/com/mycompany/quickchatapp/Login.java
@@ -0,0 +1,91 @@
+package com.mycompany.quickchatapp;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author makhu
+ */
+public class Login {
+    //data I'm going to store
+    String savedUsername;
+    String savedPassword;
+    String savedCellNumber;
+    String firstName;
+    String lastName;
+    
+    //methy 1
+     boolean checkUserName(String userName){ //checking whether the username is or less than 5 and has an underscore
+         
+         return userName.contains("_")&& userName.length() <=5;
+     }
+     //checking if all the required thingies for the password are there oh the characters whatsoever.
+     //Methy 2
+      boolean checkPasswordComplexity(String password){
+         boolean hasLength= password.length() >=8;
+         boolean hasCapital= false;
+         boolean hasNumber= false;
+         boolean hasSpecial= false;
+         
+         for(int i= 0; i < password.length(); i++){
+             char c= password.charAt(i);
+             if
+                     (Character.isUpperCase(c)){
+                 hasCapital= true;    
+             }
+             if (Character.isDigit(c)){
+                 hasNumber=true;
+             }
+             if (!Character.isLetterOrDigit(c)){
+                 hasSpecial= true;
+             }
+         }
+         return hasLength && hasCapital && hasNumber && hasSpecial;//if it has all the required thingies it will return this 
+     }
+      //Methy 3
+      //Here I'll be checking if the number starts with +27
+      //& has 10 characters + 2 so 12.
+      boolean checkCellPhoneNumber(String cellNumber){
+          //so here basically I'm saying if the number starts with +27 and is 12 characters aaaah its ncaah!! 
+          //Kazi ubani owafika nalesiZulu esibhedayo la egoli,kodwa ngisola amapedi.
+          return cellNumber.startsWith("+27")&& cellNumber.length()==12;
+      }
+      //ayke sezizongikhalela ngempelake manje, kodwa ngoba ngumzulu mina ayy bafana ngingenhla kakhulu!!
+      // sesika IEC sya register manje
+      String registerUser(String username, String password, String cellNumber){
+          //soqale sibheke igama elisetshenziswayo
+          if (!checkUserName(username)){
+              return "Username is not correctly formatted, please ensure that your username containts an underscore and "
+                      + "is no more than five characters in length"; //awuzwe nje isingisi esingaka ngingedwa
+          }
+          //manje sengibheka i password aka igamalokwendula
+          if (!checkPasswordComplexity(password)){
+              return "Cell phone number is incorrectly formatted or does not contain an international code, please correct "
+                      + "the number and try again.";
+          }
+          //makulunge konke lemininingwane izodlala kwibhokisi lesikhumbuzo
+      this.savedUsername= username;
+      this.savedPassword= password;
+      this.savedCellNumber= cellNumber;
+      
+      return " User has been registered successfully.";
+}
+      //Methy 5
+      //Login Method
+      boolean loginUser(String enteredUsername, String enteredPassword){
+          //to see if what they entered matches with what I saved
+          return enteredUsername.equals(this.savedUsername) && enteredPassword.equals(this.savedPassword);
+      }
+      String returnLoginStatus (String enteredUsername, String enteredPassword){
+          if(loginUser(enteredUsername, enteredPassword)){
+              return "Welcome" + firstName + " " + lastName + "it is great to see you again.";
+          }else{
+              return "Username or password is incorrect, please try again.";
+          }
+      }
+      }
+     
+      
---
+++ src/main/java/com/mycompany/quickchatapp/QuickChatApp.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickchatapp;
+import java.util.Scanner;
+/**
+ *
+ * @author makhu
+ */
+public class QuickChatApp {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        //my object for logging in
+        Login user= new Login();
+        
+        //lanake I'll store igama lokuqala nesbongo ukuze soba nomlayezo wokwamukela
+        System.out.println("WELCOME TO QUICKCHAT");
+        
+        System.out.println("Enter First Name:  ");
+           user.firstName= input.nextLine();
+           
+         System.out.println("Enter Last Name");  
+           user.lastName= input.nextLine();
+           
+           String username;
+           String password;
+           String cellNumber;
+           
+         //isizolokhu ibuzile uyoze ufake okufanele phela asidlali la
+         System.out.println("\n--- Registration ---");
+        do {
+            System.out.print("Enter username (must contain _ and <=5 chars e.g. kyl_1): ");
+            username = input.nextLine();
+            if (!user.checkUserName(username)) {
+                System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.");
+            }
+        } while (!user.checkUserName(username));
+        System.out.println("Username successfully captured.");
+        
+        // Solokhu sibuzile nje
+        do {
+            System.out.print("Enter password (8+ chars, caps, number, special e.g. Ch&&sec@ke99!): ");
+            password = input.nextLine();
+            if (!user.checkPasswordComplexity(password)) {
+                System.out.println("Password is not correctly formatted, please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.");
+            }
+        } while (!user.checkPasswordComplexity(password));
+        System.out.println("Password successfully captured.");
+        
+        // sisamengakho nje akujiki ndawo
+        do {
+            System.out.print("Enter cell number (+27... e.g. +27838568976): ");
+            cellNumber = input.nextLine();
+            if (!user.checkCellPhoneNumber(cellNumber)) {
+                System.out.println("Cell phone number is incorrectly formatted or does not contain an international code, please correct the number and try again.");
+            }
+        } while (!user.checkCellPhoneNumber(cellNumber));
+        System.out.println("Cell phone number successfully captured.");
+        
+        // Nakhyaaaaaa, sesibuyile ka IEC
+        String result = user.registerUser(username, password, cellNumber);
+        System.out.println("\n" + result);
+        
+        //sesiyakufakake enkundleni yokuxhumana
+        System.out.println("\n--- Login ---");
+        
+        System.out.print("Enter username to login: ");
+        String loginUname = input.nextLine();
+        
+        System.out.print("Enter password to login: ");
+        String loginPword = input.nextLine();
+        
+        // sesisebenzisa leya Methy esiyikhande ngale
+        System.out.println(user.returnLoginStatus(loginUname, loginPword));
+        
+        //ayy khohlwani, senginyuka nayo lento.
+    }
+}
